de68ccf9e66d2ed7b9bf1ab1042a6721c8cbdd0b
Ayon Bhowmick <ayonbhowmick2002@gmail.com>
2023-08-15 09:07:41 -0400

    change filename

--- fac.py
+++ fac.py
@@ -7,9 +7,10 @@ import subprocess
 
 SAVE_PATH_WIN = f"\\Users\\{os.getlogin()}\\AppData\\Roaming\\Factorio\\saves"
 SAVE_PATH_MAC = "/Library/Application Support/factorio/saves"
+FILENAME = "waffle.zip"
 
 def push():
-    shutil.copyfile(save_path, os.getcwd() + "\\waffle.zip")
+    shutil.copyfile(save_path, os.getcwd() + f"\\{FILENAME}")
     os.system("git add *")
     current_time = datetime.now(pytz.timezone("America/New_York"))
     current_time_str = current_time.strftime("%H:%M:%S")
@@ -18,17 +19,17 @@ def push():
 
 def pull():
     if out := "Already up to date." not in str(subprocess.check_output("git pull", shell=True)):
-        shutil.copyfile("./waffle.zip", save_path)
+        shutil.copyfile(f"./{FILENAME}", save_path)
     return out
 
 if __name__ == "__main__":
     if platform == "win32":
         print("Windows detected")
         assert os.path.exists(SAVE_PATH_WIN), "Path does not exist"
-        save_path = SAVE_PATH_WIN + "\\waffle.zip"
+        save_path = SAVE_PATH_WIN + F"\\{FILENAME}"
     elif platform == "darwin":
         print("Mac detected")
         assert os.path.exists(SAVE_PATH_MAC), "Path does not exist"
-        save_path = SAVE_PATH_MAC + "/waffle.zip"
+        save_path = SAVE_PATH_MAC + F"/{FILENAME}"
     if not pull():
         push()
